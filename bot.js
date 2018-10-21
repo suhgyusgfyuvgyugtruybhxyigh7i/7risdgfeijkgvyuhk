5fbbe8775dc13eedb7d39c5de7c9395fac06b6c8
suhgyusgfyuvgyugtruybhxyigh7i <44337427+suhgyusgfyuvgyugtruybhxyigh7i@users.noreply.github.com>
2018-10-21 14:07:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "-";
+const prefix = "*";
 client.on('ready', () => {
   client.user.setGame('Type *help','https://www.twitch.tv/peery13');
   console.log('---------------');
@@ -928,6 +928,89 @@ client.on('message', (message) => {
         });
     }
 });
+
+
+client.on('message',message =>{
+    var prefix = "*";
+    if(message.content.startsWith(prefix + 'top')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+  .setThumbnail("https://media.discordapp.net/attachments/477570106755383307/479229377037598720/22713057_151850495552450_709700562_o.jpg?width=201&height=201")
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
