e9802d0a09a6a6d555510ea136fe65b3063ddcea
suhgyusgfyuvgyugtruybhxyigh7i <44337427+suhgyusgfyuvgyugtruybhxyigh7i@users.noreply.github.com>
2018-10-21 17:23:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -203,6 +203,17 @@ client.on('message', message => {
 
 ❖ *invs ➾ يعطيك رابط السيرفر الذي كتبت في الامر
 
+╔[❖════════════❖]╗
+                    أوامر الالعاب
+╚[❖════════════❖]╝
+
+❖ * لعبه صراحه ➾ صراحه
+
+❖ *rps ➾ لعبة حجره ورقه مقص
+
+❖ *امثال ➾ lلعبة امثله
+
+❖ *ايموجي ➾ lلعبة ايموجي
 
 ==================================================================
 
@@ -858,18 +869,260 @@ client.on('message', async message => {
 
 
 
+const Sra7a = [
+            'صراحه  |  صوتك حلوة؟',
+            'صراحه  |  التقيت الناس مع وجوهين؟',
+            'صراحه  |  شيء وكنت تحقق اللسان؟',
+            'صراحه  |  أنا ��خص ضعيف عندما؟',
+            'صراحه  |  هل ترغب في إظهار حبك ومرفق لشخص أو رؤية هذا الضعف؟',
+            'صراحه  |  يدل على أن الكذب مرات تكون ضرورية شي؟',
+            'صراحه  |  أشعر بالوحدة على الرغم من أنني تحيط بك كثيرا؟',
+            'صراحه  |  كيفية الكشف عن من يكمن عليك؟',
+            'صراحه  |  إذا حاول شخص ما أن يكرهه أن يقترب منك ويهتم بك تعطيه فرصة؟',
+            'صراحه  |  أشجع شيء حلو في حياتك؟',
+            'صراحه  |  طريقة جيدة يقنع حتى لو كانت الفكرة خاطئة" توافق؟',
+            'صراحه  |  كيف تتصرف مع من يسيئون فهمك ويأخذ على ذهنه ثم ينتظر أن يرفض؟',
+            'صراحه  |  التغيير العادي عندما يكون الشخص الذي يحبه؟',
+            'صراحه  |  المواقف الصعبة تضعف لك ولا ترفع؟',
+            'صراحه  |  نظرة و يفسد الصداقة؟',
+            'صراحه  |  ‏‏إذا أحد قالك كلام سيء بالغالب وش تكون ردة فعلك؟',
+            'صراحه  |  شخص معك بالحلوه والمُره؟',
+            'صراحه  |  ‏هل تحب إظهار حبك وتعلقك بالشخص ��م ترى ذلك ضعف؟',
+            'صراحه  |  تأخذ بكلام اللي ينصحك ولا تسوي اللي تبي؟',
+            'صراحه  |  وش تتمنى الناس تعرف عليك؟',
+            'صراحه  |  ابيع المجرة عشان؟',
+            'صراحه  |  أحيانا احس ان الناس ، كمل؟',
+            'صراحه  |  مع مين ودك تنام اليوم؟',
+            'صراحه  |  صدفة العمر الحلوة هي اني؟',
+            'صراحه  |  الكُره العظيم دايم يجي بعد حُب قوي " تتفق؟',
+            'صراحه  |  صفة تحبها في نفسك؟',
+            'صراحه  |  ‏الفقر فقر العقول ليس الجيوب " ، تتفق؟',
+            'صراحه  |  تصلي صلواتك الخمس كلها؟',
+            'صراحه  |  ‏تجامل أحد على راحتك؟',
+            'صراحه  |  اشجع شيء سويتة بحياتك؟',
+            'صراحه  |  وش ناوي تسوي اليوم؟',
+            'صراحه  |  وش شعورك لما تشوف المطر؟',
+            'صراحه  |  غيرتك هاديه ولا تسوي مشاكل؟',
+            'صراحه  |  ما اكثر شي ندمن عليه؟',
+            'صراحه  |  اي الدول تتمنى ان تزورها؟',
+            'صراحه  |  متى اخر مره بكيت؟',
+            'صراحه  |  تقيم حظك ؟ من عشره؟',
+            'صراحه  |  هل تعتقد ان حظك سيئ؟',
+            'صراحه  |  شـخــص تتمنــي الإنتقــام منـــه؟',
+            'صراحه  |  كلمة تود سماعها كل يوم؟',
+            'صراحه  |  **هل تُتقن عملك أم تشعر بالممل؟',
+            'صراحه  |  هل قمت بانتحال أحد الشخصيات لتكذب على من حولك؟',
+            'صراحه  |  متى آخر مرة قمت بعمل مُشكلة كبيرة وتسببت في خسائر؟',
+            'صراحه  |  ما هو اسوأ خبر سمعته بحياتك؟',
+            '‏صراحه | هل جرحت شخص تحبه من قبل ؟',
+            'صراحه  |  ما هي العادة التي تُحب أن تبتعد عنها؟',
+            '‏صراحه | هل تحب عائلتك ام تكرههم؟',
+            '‏صراحه  |  من هو الشخص الذي يأتي في قلبك بعد الله – سبحانه وتعالى- ورسوله الكريم – صلى الله عليه وسلم؟',
+            '‏صراحه  |  هل خجلت من نفسك من قبل؟',
+            '‏صراحه  |  ما هو ا الحلم  الذي لم تستطيع ان تحققه؟',
+            '‏صراحه  |  ما هو الشخص الذي تحلم به كل ليلة؟',
+            '‏صراحه  |  هل تعرضت إلى موقف مُ��رج جعلك تكره صاحبهُ؟',
+             '‏صراحه  |  هل قمت بالبكاء أمام من تُحب؟',
+            '‏صراحه  |  ماذا تختار حبيبك أم صديقك؟',
+            '‏صراحه  | هل حياتك سعيدة أم حزينة؟',
+            'صراحه  |  ما هي أجمل سنة عشتها بحياتك؟',
+            '‏صراحه  |  ما هو عمرك الحقيقي؟',
+            '‏صراحه  |  ما اكثر شي ندمن عليه؟',
+            'صراحه  |  ما هي أمنياتك المُستقبلية؟‏',
+       ]
+          client.on('message', message => {
+			  	var prefix = "*"
+        if (message.content.startsWith(prefix + 'صراحه')) {
+            if(!message.channel.guild) return message.reply('** This command only for servers **');
+         var client= new Discord.RichEmbed()
+         .setTitle("Alpha")
+         .setColor('RANDOM')
+         .setDescription(`${Sra7a[Math.floor(Math.random() * Sra7a.length)]}`)
+         .setImage("https://cdn.discordapp.com/attachments/371269161470525444/384103927060234242/125.png")
+                         .setTimestamp()
+       
+          message.channel.sendEmbed(client);
+          message.react("??")
+        }
+       });
 
 
 
+client.on("message", function(message) {
+	var prefix = "*";
+   if(message.content.startsWith(prefix + "rps")) {
+    let messageArgs = message.content.split(" ").slice(1).join(" ");
+    let messageRPS = message.content.split(" ").slice(2).join(" ");
+    let arrayRPS = ['**# - Rock**','**# - Paper**','**# - Scissors**'];
+    let result = `${arrayRPS[Math.floor(Math.random() * arrayRPS.length)]}`;
+    var RpsEmbed = new Discord.RichEmbed()
+    .setAuthor(message.author.username)
+    .setThumbnail(message.author.avatarURL)
+    .addField("Rock","ًں‡·",true)
+    .addField("Paper","ًں‡µ",true)
+    .addField("Scissors","ًں‡¸",true)
+    message.channel.send(RpsEmbed).then(msg => {
+        msg.react(' ًں‡·')
+        msg.react("ًں‡¸")
+        msg.react("ًں‡µ")
+.then(() => msg.react('ًں‡·'))
+.then(() =>msg.react('ًں‡¸'))
+.then(() => msg.react('ًں‡µ'))
+let reaction1Filter = (reaction, user) => reaction.emoji.name === 'ًں‡·' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === 'ًں‡¸' && user.id === message.author.id;
+let reaction3Filter = (reaction, user) => reaction.emoji.name === 'ًں‡µ' && user.id === message.author.id;
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+	    
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+let reaction3 = msg.createReactionCollector(reaction3Filter, { time: 12000 });
+reaction1.on("collect", r => {
+        message.channel.send(result)
+})
+reaction2.on("collect", r => {
+        message.channel.send(result)
+})
+reaction3.on("collect", r => {
+        message.channel.send(result)
+})
+
+    })
+}
+});
 
 
 
+client.on('message', message => {
+    if (!points[message.author.id]) points[message.author.id] = { 
+        points: 0,
+      };
+    if (message.content.startsWith(prefix + 'ايموجي')) { 
+        if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+    
+    const type = require('./emoje/emoje.json'); 
+    const item = type[Math.floor(Math.random() * type.length)]; 
+    const filter = response => { 
+        return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+    };
+   message.channel.send({embed: new Discord.RichEmbed().setTitle('لديك ثانيه للاجابه بالجواب الصحيح').setThumbnail(`${item.type}`)}).then(function(m) {
+             setTimeout(function() {
+m.edit({embed: new Discord.RichEmbed().setTitle('لديك 15 ثانيه للاجابه بالجواب الصحيح').setThumbnail('https://images-ext-2.discordapp.net/external/lLOYcLfSQaNo_5Ex0I-gBD5lIW-FfRXO-W_-ZxSpYLA/https/i.imgur.com/iReHvIZ.png?width=100&height=100')})
+             }, 1000)
+            message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+            .then((collected) => {
+            message.channel.send(`${collected.first().author} ✅ **لقد قمت بكتابة الايموجي بالوقت المناسب**`);
+            console.log(`[Game] ${collected.first().author} Answered with the correct answer`);
+                let won = collected.first().author;
+                points[won.id].points++;
+              })
+              .catch(collected => { 
+                message.channel.send(`:x: **لم يقم أحد بكتابة الايموجي بالوقت المناسب**`);
+                console.log(`[Game] ماحد قال الاجابه`);
+                    })
+            })
+    }
+    });
+
+//==================== JSON
+
+[
+    {
+        "type": "https://emojipedia-us.s3.amazonaws.com/thumbs/72/twitter/134/face-with-look-of-triumph_1f624.png",
+        "answers": ["ًںک¤"]
+    },
+    {
+        "type": "https://emojipedia-us.s3.amazonaws.com/thumbs/120/twitter/134/baby_1f476.png",
+        "answers": ["ًں‘¶"]
+    },
+    {
+        "type": "https://emojipedia-us.s3.amazonaws.com/thumbs/120/twitter/134/japanese-goblin_1f47a.png",
+        "answers": ["ًں‘؛"]
+    },
+    {
+        "type": "https://emojipedia-us.s3.amazonaws.com/thumbs/120/twitter/134/see-no-evil-monkey_1f648.png",
+        "answers": ["ًں™ˆ"]
+    },
+        {
+        "type": "https://emojipedia-us.s3.amazonaws.com/thumbs/120/twitter/134/flag-for-yemen_1f1fe-1f1ea.png",
+        "answers": ["ًں‡¾ًں‡ھ"]
+    },
+        {
+        "type": "https://emojipedia-us.s3.amazonaws.com/thumbs/120/twitter/134/comet_2604.png",
+        "answers": ["âک„ï¸ڈ"]
+    },
+        {
+        "type": "https://emojipedia-us.s3.amazonaws.com/thumbs/120/twitter/134/trident-emblem_1f531.png",
+        "answers": ["ًں”±"]
+    },
+        {
+        "type": "https://emojipedia-us.s3.amazonaws.com/thumbs/120/twitter/134/fleur-de-lis_269c.png",
+        "answers": ["âڑœï¸ڈ"]
+    }
+    
+]
 
 
-
-
-
-
+client.on('message', message => {
+    if (message.content == "*امثال") {
+        var x = ["أذا ذل رويال فهو ...",
+"الإتحاد ...",
+"الناس سواسية كأسنان ...",
+"ما أشبه الليله",
+"البعد ...",
+"الماء أهون موجود وأعز ...",
+"الهزيمة تحل ...",
+"العقل ...",
+"البطنة تزيل ...",
+"اللبيب بالإشارة ...",
+"اخطب لابنتك ولا تخطب ...",
+"أعز من الولد ولد ...",
+"القرد في عين أمه ...",
+"الكتاب يقرأ من ...",
+"آخر الحياة ...",
+"أكرم نفسك عن كل",
+"العز في نواصي",
+];
+        var x2 = ['ذليل',
+        "قوة",
+        "المشط",
+        "بالبارحة",
+        "جفاء",
+        "مفقود",
+        "العزيمة",
+        "زينة",
+        "الفطنة",
+        "يفهم",
+        "لابنك",
+        "الولد",
+        "غزال",
+        "عنوانه",
+        "الموت",
+        "دنيء",
+        "الخيل",
+        
+        
+        
+        
+        ];
+        
+        var x3 = Math.floor(Math.random()*x.length)
+        message.channel.send(` اكمل المثل التآلي :  __**${x[x3]}**__ ؟
+    لديك 30 ثآنية للإجآبة `).then(msg1=> {
+            var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
+                maxMatches : 1,
+                time : 30000,
+                errors : ['time']
+            })
+        r.catch(() => {
+            return message.channel.send(`:negative_squared_cross_mark: لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح 
+            الإجآبة الصحيحةة هي __**${x2[x3]}**__`)
+        })
+        
+        r.then((collected)=> {
+            message.channel.send(`${collected.first().author} لقد قمت بكتابة الجواب الصحيح بالوقت المناسب  `);
+        })
+        })
+    }
+})
 
 
 
